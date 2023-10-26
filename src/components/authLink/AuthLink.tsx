9967414ef1d876c308b6becbeec600318bfa7d4f
nauvalyusufaddairy <nauvalyusufad@gmail.com>
2023-10-26 12:56:40 +0700

    categorylist is done 1:05:00

--- src/components/authLink/AuthLink.tsx
+++ src/components/authLink/AuthLink.tsx
@@ -11,18 +11,35 @@ export default function AuthLink() {
     <div>
       {/* @ts-ignore */}
       {status === "notauthenticated" ? (
-        <Link href={"/login"}>Login</Link>
+        <Link href={"/login"} className={styles.link}>
+          Login
+        </Link>
       ) : (
         <>
           <Link href={"/write"}>Write</Link>
           <span className={styles.link}>logout</span>
         </>
       )}
-      <div className={styles.burger}>
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
         <div className={styles.line}></div>
         <div className={styles.line}></div>
         <div className={styles.line}></div>
       </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href={"/"}>Homepage</Link>
+          <Link href={"/"}>About</Link>
+          <Link href={"/"}>Contact</Link>
+          {status === "notauthenticated" ? (
+            <Link href={"/login"}>Login</Link>
+          ) : (
+            <>
+              <Link href={"/write"}>Write</Link>
+              <span className={styles.link}>logout</span>
+            </>
+          )}
+        </div>
+      )}
     </div>
   );
 }
