1b898a1f60908f945c3cffe01020a49c85272032
nauvalyusufaddairy <nauvalyusufad@gmail.com>
2023-10-31 18:55:52 +0700

    signin function error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -16,11 +16,18 @@ const LoginPage = () => {
   if (status === "authenticated") {
     router.push("/");
   }
+  const handleSignin = async () => {
+    const res = await signIn("google");
+
+    if (res?.error) {
+      console.log("error", res);
+    }
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.socialButton} onClick={() => signIn("google")}>
+        <div className={styles.socialButton} onClick={() => handleSignin()}>
           Sign in with Google
         </div>
         <div className={styles.socialButton}>Sign in with Github</div>
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,8 +1,14 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { AuthOptions } from "next-auth";
+import { Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "./connect";
+// const prisma = new PrismaClient();
 
 export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
